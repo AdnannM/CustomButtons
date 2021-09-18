8254dbf550caba9a2185254d81dcf940df93011c
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-09-18 21:04:55 +0200

    @IBDesignable and @IBInspectable

---
+++ CustomButtons/FansyButtons.swift
@@ -0,0 +1,17 @@
+//
+//  FansyButtons.swift
+//  CustomButtons
+//
+//  Created by Adnann Muratovic on 18.09.21.
+//
+
+import UIKit
+
+@IBDesignable
+class FansyButtons: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            
+        }
+    }
+}
